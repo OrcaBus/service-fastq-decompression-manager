87826ea8822580dfc1c96ac9b8c03480c42888b3
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-08-21 16:19:03 +1000

    Tabs to spaces Resolve tabs-to-spaces after rebase

--- app/ecs/ora_decompression/docker-entrypoint.sh
+++ app/ecs/ora_decompression/docker-entrypoint.sh
@@ -86,85 +86,85 @@ ora_key="$( \
   uv run python3 -c "from urllib.parse import urlparse; print(urlparse(\"${INPUT_ORA_URI}\").path.lstrip(\"/\"))"
 )"
 s3_object_id="$( \
-	curl \
-	  --request GET \
-	  --fail --silent --location --show-error \
-	  --header "Accept: application/json " \
-	  --header "Authorization: Bearer ${ORCABUS_TOKEN}" \
-	  --url "https://file.${HOSTNAME}/api/v1/s3?bucket=${ora_bucket}&key=${ora_key}&currentState=true" | \
-	jq --raw-output \
-      '
-        if .results | length == 0 then
-		  error("No results found for the given bucket and key")
-		elif .results | length > 1 then
-		  error("Multiple results found for the given bucket and key")
-		else
-		  .results[0].s3ObjectId
-		end
-      '
+  curl \
+    --request GET \
+    --fail --silent --location --show-error \
+    --header "Accept: application/json " \
+    --header "Authorization: Bearer ${ORCABUS_TOKEN}" \
+    --url "https://file.${HOSTNAME}/api/v1/s3?bucket=${ora_bucket}&key=${ora_key}&currentState=true" | \
+  jq --raw-output \
+    '
+      if .results | length == 0 then
+        error("No results found for the given bucket and key")
+      elif .results | length > 1 then
+        error("Multiple results found for the given bucket and key")
+      else
+        .results[0].s3ObjectId
+      end
+    ' \
 )"
 
 # Get the presigned url
 echo_stderr "Collecting the presigned URL for the input ora file"
 presigned_url="$(  \
-	curl \
-	  --request GET \
-	  --fail --silent --location --show-error \
-	  --header "Accept: application/json " \
-	  --header "Authorization: Bearer ${ORCABUS_TOKEN}" \
-	  --url "https://file.${HOSTNAME}/api/v1/s3/presign/${s3_object_id}" | \
-	jq --raw-output
+  curl \
+    --request GET \
+    --fail --silent --location --show-error \
+    --header "Accept: application/json " \
+    --header "Authorization: Bearer ${ORCABUS_TOKEN}" \
+    --url "https://file.${HOSTNAME}/api/v1/s3/presign/${s3_object_id}" | \
+  jq --raw-output
 )"
 
 # Download + Upload the ora file as a gzipped compressed file
 if [[ "${JOB_TYPE}" == "ORA_DECOMPRESSION" ]]; then
   # Get the sampling parameter
   if [[ "${SAMPLING}" == "true" ]]; then
-	# If sampling is 1, then seqtk will recognise as the number of reads to return
-	# and then only return only the first read
-	if [[ "${TOTAL_READ_COUNT}" -le "${MAX_READS}" ]]; then
-	  echo_stderr "Turning off sampling as total read count is less than the maximum reads"
-	  SAMPLING="false"
-	else
-	  SAMPLING_PROPORTION="$( \
+  # If sampling is 1, then seqtk will recognise as the number of reads to return
+  # and then only return only the first read
+  if [[ "${TOTAL_READ_COUNT}" -le "${MAX_READS}" ]]; then
+    echo_stderr "Turning off sampling as total read count is less than the maximum reads"
+    SAMPLING="false"
+  else
+    SAMPLING_PROPORTION="$( \
       jq --null-input --raw-output \
         --argjson maxReads "${MAX_READS}" \
         --argjson totalReadCount "${TOTAL_READ_COUNT}" \
         '
-        	if $maxReads > $totalReadCount then
-				1.0
-			else
-				# Calculate the sampling proportion as a percentage
-				# This is the maximum reads divided by the total read count
-				# rounded to 2 decimal places
-				(
-					(( 100 * $maxReads) / $totalReadCount ) | round
-				) / 100
-			end
+          if $maxReads > $totalReadCount then
+            1.0
+          else
+            # Calculate the sampling proportion as a percentage
+            # This is the maximum reads divided by the total read count
+            # rounded to 2 decimal places
+            (
+               (( 100 * $maxReads) / $totalReadCount ) | round
+            ) / 100
+          end
         '
-	  )"
-	  echo_stderr "Sampling Proportion is ${SAMPLING_PROPORTION}"
-	fi
+    )"
+    echo_stderr "Sampling Proportion is ${SAMPLING_PROPORTION}"
+  fi
   fi
 
   if [[ "${MAX_READS}" -gt 0 ]]; then
-	line_count="$( \
-	  jq --null-input --raw-output \
-	    --argjson maxReads "${MAX_READS}" \
-	    '
-	      $maxReads * 4
-	    ' \
-	)"
+  line_count="$( \
+    jq --null-input --raw-output \
+      --argjson maxReads "${MAX_READS}" \
+      '
+        $maxReads * 4
+      ' \
+  )"
   fi
 
   # Get the icav2 accession credentials if required
   if [[ ! "${OUTPUT_GZIP_URI}" =~ s3://${S3_DECOMPRESSION_BUCKET}/ ]]; then
-	# Set AWS credentials access for aws s3 cp
-  	echo_stderr "Collecting the AWS S3 Access credentials"
-  	aws_s3_access_creds_json_str="$( \
-	    uv run python3 scripts/get_icav2_aws_credentials_access.py \
-	      "$(dirname "${OUTPUT_GZIP_URI}")/"
-  	)";
+  # Set AWS credentials access for aws s3 cp
+    echo_stderr "Collecting the AWS S3 Access credentials"
+    aws_s3_access_creds_json_str="$( \
+      uv run python3 scripts/get_icav2_aws_credentials_access.py \
+        "$(dirname "${OUTPUT_GZIP_URI}")/"
+    )";
   fi
 
   # Use a file descriptor to emulate the ora file
@@ -188,72 +188,72 @@ if [[ "${JOB_TYPE}" == "ORA_DECOMPRESSION" ]]; then
   # 6b. If the output gzip uri is in the S3_DECOMPRESSION_BUCKET, upload to S3 using aws s3 cp with task role credentials
   ( \
     (
-	  wget \
-		--quiet \
-		--output-document /dev/stdout \
-		"${presigned_url}" | \
-	  /usr/local/bin/orad \
-		--raw \
-		--stdout \
-		--ora-reference "${ORADATA_PATH}" \
-		- | \
-	  (
-		if [[ "${SAMPLING}" == "true" ]]; then
-		  seqtk sample \
-			-s "${RANDOM_SAMPLING_SEED}" \
-			- \
-			"${SAMPLING_PROPORTION}"
-		else
-		  cat
-		fi
-	  ) \
-	) || \
-	true
+      wget \
+        --quiet \
+        --output-document /dev/stdout \
+      "${presigned_url}" | \
+      /usr/local/bin/orad \
+        --raw \
+        --stdout \
+        --ora-reference "${ORADATA_PATH}" \
+        - | \
+      (
+        if [[ "${SAMPLING}" == "true" ]]; then
+          seqtk sample \
+          -s "${RANDOM_SAMPLING_SEED}" \
+          - \
+          "${SAMPLING_PROPORTION}"
+        else
+          cat
+        fi
+      ) \
+    ) || \
+    true
   ) | \
   (
-  	if [[ "${MAX_READS}" -gt 0 ]]; then
-	  head -n "${line_count}"
-	else
-	  cat
-	fi
+    if [[ "${MAX_READS}" -gt 0 ]]; then
+    head -n "${line_count}"
+  else
+    cat
+  fi
   ) | \
   (
-  	pigz \
-	  --stdout \
-	  --fast \
-	  --processes 4
+    pigz \
+    --stdout \
+    --fast \
+    --processes 4
   ) | \
   (
     if [[ ! "${OUTPUT_GZIP_URI}" =~ s3://${S3_DECOMPRESSION_BUCKET}/ ]]; then
-	  AWS_ACCESS_KEY_ID="$( \
-		jq -r '.AWS_ACCESS_KEY_ID' <<< "${aws_s3_access_creds_json_str}"
-	  )" \
-	  AWS_SECRET_ACCESS_KEY="$( \
-		jq -r '.AWS_SECRET_ACCESS_KEY' <<< "${aws_s3_access_creds_json_str}"
-	  )" \
-	  AWS_SESSION_TOKEN="$( \
-		jq -r '.AWS_SESSION_TOKEN' <<< "${aws_s3_access_creds_json_str}"
-	  )" \
-	  AWS_REGION="$( \
-		jq -r '.AWS_REGION' <<< "${aws_s3_access_creds_json_str}"
-	  )" \
-	  aws s3 cp \
-		--expected-size="${GZIP_COMPRESSION_SIZE_IN_BYTES}" \
-		--sse=AES256 \
-		- \
-		"$( \
-		  uv run python3 scripts/get_s3_uri.py \
-			"$(dirname "${OUTPUT_GZIP_URI}")/" \
-		)$( \
-		  basename "${OUTPUT_GZIP_URI}" \
-		)"
-	else
-	  aws s3 cp \
-		--expected-size="${GZIP_COMPRESSION_SIZE_IN_BYTES}" \
-		--sse=AES256 \
-		- \
-		"${OUTPUT_GZIP_URI}"
-	fi
+      AWS_ACCESS_KEY_ID="$( \
+        jq -r '.AWS_ACCESS_KEY_ID' <<< "${aws_s3_access_creds_json_str}"
+      )" \
+      AWS_SECRET_ACCESS_KEY="$( \
+        jq -r '.AWS_SECRET_ACCESS_KEY' <<< "${aws_s3_access_creds_json_str}"
+      )" \
+      AWS_SESSION_TOKEN="$( \
+        jq -r '.AWS_SESSION_TOKEN' <<< "${aws_s3_access_creds_json_str}"
+      )" \
+      AWS_REGION="$( \
+        jq -r '.AWS_REGION' <<< "${aws_s3_access_creds_json_str}"
+      )" \
+      aws s3 cp \
+        --expected-size="${GZIP_COMPRESSION_SIZE_IN_BYTES}" \
+        --sse=AES256 \
+        - \
+        "$( \
+          uv run python3 scripts/get_s3_uri.py \
+          "$(dirname "${OUTPUT_GZIP_URI}")/" \
+        )$( \
+          basename "${OUTPUT_GZIP_URI}" \
+        )"
+    else
+      aws s3 cp \
+        --expected-size="${GZIP_COMPRESSION_SIZE_IN_BYTES}" \
+        --sse=AES256 \
+        - \
+        "${OUTPUT_GZIP_URI}"
+    fi
   )
   echo_stderr "Stream and upload of decompression complete"
 
@@ -261,18 +261,18 @@ if [[ "${JOB_TYPE}" == "ORA_DECOMPRESSION" ]]; then
   jq --null-input --raw-output \
     --arg gzip_file_uri "${OUTPUT_GZIP_URI}" \
     --arg ingest_id "${ORA_INGEST_ID}" \
-  	'
-  		{
-  			"ingestId": $ingest_id,
-  			"gzipFileUri": $gzip_file_uri
-  		}
-  	' > output.json
+    '
+      {
+        "ingestId": $ingest_id,
+        "gzipFileUri": $gzip_file_uri
+      }
+    ' > output.json
 
   echo_stderr "Uploading metadata"
   aws s3 cp \
-	--sse=AES256 \
-	output.json \
-	"${OUTPUT_METADATA_URI}"
+    --sse=AES256 \
+    output.json \
+    "${OUTPUT_METADATA_URI}"
   echo_stderr "Metadata upload complete"
 
   # Remove the output.json file
@@ -284,63 +284,62 @@ elif [[ "${JOB_TYPE}" == "GZIP_FILESIZE_CALCULATION" ]]; then
   echo_stderr "Calculating the gzipped file size"
   gzip_file_size_in_bytes="$( \
     ( \
-	  wget \
-		--quiet \
-		--output-document /dev/stdout \
-		"${presigned_url}" || \
-	  true
-	) | \
-	(
-	  /usr/local/bin/orad \
-		--raw \
-		--stdout \
-		--ora-reference "${ORADATA_PATH}" \
-		- || \
-	  true
-	) | \
-	(
-	    # If total read count is set we (and more than the max reads to downlaod)
-	    # we can instead calculate the gzipped file size
-	    # By only reading the first N reads
-	    # Then calculating the gzipped file size from the ratio of the total read count
-	    # And the gzipped file size
-		if [[ "${TOTAL_READ_COUNT}" -gt "${MAX_READS_IF_TOTAL_READ_COUNT_IS_SET}" ]]; then
-		  head -n "$(( "${MAX_READS_IF_TOTAL_READ_COUNT_IS_SET}" * 4 ))"
-		else
-		  cat
-		fi
-	) | \
-	(
-	  pigz \
-		--stdout \
-		--fast
-	) | \
-	wc -c
+      wget \
+      --quiet \
+      --output-document /dev/stdout \
+      "${presigned_url}" || \
+      true
+    ) | \
+    (
+      /usr/local/bin/orad \
+      --raw \
+      --stdout \
+      --ora-reference "${ORADATA_PATH}" \
+      - || \
+      true
+    ) | \
+    (
+        # If total read count is set we (and more than the max reads to downlaod)
+        # we can instead calculate the gzipped file size
+        # By only reading the first N reads
+        # Then calculating the gzipped file size from the ratio of the total read count
+        # And the gzipped file size
+      if [[ "${TOTAL_READ_COUNT}" -gt "${MAX_READS_IF_TOTAL_READ_COUNT_IS_SET}" ]]; then
+        head -n "$(( "${MAX_READS_IF_TOTAL_READ_COUNT_IS_SET}" * 4 ))"
+      else
+        cat
+      fi
+    ) | \
+    (
+      pigz \
+      --stdout \
+      --fast
+    ) | \
+    wc -c
   )"
 
   # Now if the total read count is set, we can calculate the gzipped file size
   # From the ratio of the total read count
   if [[ "${TOTAL_READ_COUNT}" -gt "${MAX_READS_IF_TOTAL_READ_COUNT_IS_SET}" ]]; then
-	gzip_file_size_in_bytes="$(( gzip_file_size_in_bytes * TOTAL_READ_COUNT / MAX_READS_IF_TOTAL_READ_COUNT_IS_SET ))"
+    gzip_file_size_in_bytes="$(( gzip_file_size_in_bytes * TOTAL_READ_COUNT / MAX_READS_IF_TOTAL_READ_COUNT_IS_SET ))"
   fi
-
   echo_stderr "Gzipped file size is ${gzip_file_size_in_bytes} bytes"
 
   jq --null-input --raw-output \
     --argjson gzip_file_size_in_bytes "${gzip_file_size_in_bytes}" \
     --arg ingest_id "${ORA_INGEST_ID}" \
-  	'
-  		{
-  			"ingestId": $ingest_id,
-  			"gzipFileSizeInBytes": $gzip_file_size_in_bytes
-  		}
-  	' > output.json
+    '
+      {
+        "ingestId": $ingest_id,
+        "gzipFileSizeInBytes": $gzip_file_size_in_bytes
+      }
+    ' > output.json
 
   echo_stderr "Uploading metadata"
   aws s3 cp \
-	--sse=AES256 \
-	output.json \
-	"${OUTPUT_METADATA_URI}"
+    --sse=AES256 \
+    output.json \
+    "${OUTPUT_METADATA_URI}"
   echo_stderr "Metadata upload complete"
 
   # Remove the output.json file
@@ -351,35 +350,35 @@ elif [[ "${JOB_TYPE}" == "RAW_MD5SUM_CALCULATION" ]]; then
   # Download the file and pipe through orad
   echo_stderr "Calculating the raw md5sum"
   md5sum_str="$( \
-  	wget \
-  	  --quiet \
-  	  --output-document /dev/stdout \
-  	  "${presigned_url}" | \
-  	/usr/local/bin/orad \
-  	  --raw \
-  	  --stdout \
-  	  --ora-reference "${ORADATA_PATH}" \
-  	  - | \
-  	md5sum | \
-  	cut -d' ' -f1
+    wget \
+      --quiet \
+      --output-document /dev/stdout \
+      "${presigned_url}" | \
+    /usr/local/bin/orad \
+      --raw \
+      --stdout \
+      --ora-reference "${ORADATA_PATH}" \
+      - | \
+    md5sum | \
+    cut -d' ' -f1
   )"
 
   # Write the md5sum (and linked ora ingest id) to a file
   jq --null-input --raw-output \
     --arg md5sum_str "${md5sum_str}" \
     --arg ingest_id "${ORA_INGEST_ID}" \
-  	'
-  		{
-  			"ingestId": $ingest_id,
-  			"rawMd5sum": $md5sum_str
-  		}
-  	' > output.json
+    '
+      {
+        "ingestId": $ingest_id,
+        "rawMd5sum": $md5sum_str
+      }
+    ' > output.json
 
   echo_stderr "Uploading metadata"
   aws s3 cp \
-	--sse=AES256 \
-	output.json \
-	"${OUTPUT_METADATA_URI}"
+    --sse=AES256 \
+    output.json \
+    "${OUTPUT_METADATA_URI}"
   echo_stderr "Metadata upload complete"
 
   # Remove the output.json file
@@ -389,16 +388,16 @@ elif [[ "${JOB_TYPE}" == "READ_COUNT_CALCULATION" ]]; then
   # Download the file and pipe through orad
   echo_stderr "Calculating the read count"
   line_count="$( \
-  	wget \
-  	  --quiet \
-  	  --output-document /dev/stdout \
-  	  "${presigned_url}" | \
-  	/usr/local/bin/orad \
-  	  --raw \
-  	  --stdout \
-  	  --ora-reference "${ORADATA_PATH}" \
-  	  - | \
-  	wc -l
+    wget \
+      --quiet \
+      --output-document /dev/stdout \
+      "${presigned_url}" | \
+    /usr/local/bin/orad \
+      --raw \
+      --stdout \
+      --ora-reference "${ORADATA_PATH}" \
+      - | \
+    wc -l
   )"
 
   # Calculate the read count
@@ -406,20 +405,20 @@ elif [[ "${JOB_TYPE}" == "READ_COUNT_CALCULATION" ]]; then
 
   # Write the read count (and linked ora ingest id) to a file
   jq --null-input --raw-output \
-	--argjson read_count "${read_count}" \
-	--arg fastq_id "${FASTQ_ID}" \
-  	'
-  		{
-  			"fastqId": $fastq_id,
-  			"readCount": $read_count
-  		}
-  	' > output.json
+    --argjson read_count "${read_count}" \
+    --arg fastq_id "${FASTQ_ID}" \
+      '
+        {
+          "fastqId": $fastq_id,
+          "readCount": $read_count
+        }
+      ' > output.json
 
   echo_stderr "Uploading metadata"
   aws s3 cp \
-	--sse=AES256 \
-	output.json \
-	"${OUTPUT_METADATA_URI}"
+    --sse=AES256 \
+    output.json \
+    "${OUTPUT_METADATA_URI}"
   echo_stderr "Metadata upload complete"
 
   # Remove the output.json file
